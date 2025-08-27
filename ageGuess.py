449ce24d47248e07de49694e6580959cc2ffd423
Hannah Martin <hannahemartin25@gmail.com>
2025-08-27 10:44:38 -0600

    improved user interaction

--- ageGuess.py
+++ ageGuess.py
@@ -1,11 +1,11 @@
 import random
-print("Hi, I am going to try to guess your age")
+print("Hi, I am going to try to guess your age.")
 name = input("What is your name? ")
 
 age = 1
 while age == 1:
     guess = random.randrange(15, 30)
-    if (input(guess) == 'y'):
+    if (input(str(guess) + "? y/n: ") == 'y'):
         age = guess
     else:
         print("Rats.")
